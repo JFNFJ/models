23cae8e05be7bb796a2aec17de095431866eb82d
Federico Bonisconti <federico.bonisconti@mercadolibre.com>
2018-04-30 15:31:31 -0300

    added some experimental things

--- app/comment_filtering.py
+++ app/comment_filtering.py
@@ -2,14 +2,20 @@
 
 from __future__ import absolute_import, print_function
 
-from load_data import load_data
+# from load_data import load_data
 
-from keras.preprocessing import sequence
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation
-from keras.layers import Embedding
-from keras.layers import Conv1D, GlobalMaxPooling1D
-from keras.datasets import imdb
+# from keras.preprocessing import sequence
+# from keras.models import Sequential
+# from keras.layers import Dense, Dropout, Activation
+# from keras.layers import Embedding
+# from keras.layers import Conv1D, GlobalMaxPooling1D
+from keras.layers import Input, Dense, Embedding, Conv2D, MaxPool2D
+from keras.layers import Reshape, Flatten, Dropout, Concatenate
+from keras.callbacks import ModelCheckpoint
+from keras.optimizers import Adam
+from keras.models import Model
+from data_helpers import load_data
+from sklearn.model_selection import train_test_split
 
 
 
@@ -23,55 +29,98 @@ hidden_dims = 250
 epochs = 2
 
 # print('Loading data...')
-(x_train, y_train), (x_test, y_test) = load_data(path='../data/filtering_comments.npz', num_words=max_features)
-# (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
-print(len(x_train), 'train sequences')
-print(len(x_test), 'test sequences')
+x, y, vocabulary, vocabulary_inv = load_data("/Users/fbonisconti/my-files/proyecto/models/data/rt-polarity.pos", "/Users/fbonisconti/my-files/proyecto/models/data/rt-polarity.neg")
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+print(vocabulary.keys())
+sequence_length = x.shape[1]
+sequence_length = x.shape[1] # 56
+vocabulary_size = len(vocabulary_inv) # 18765
+embedding_dim = 256
+filter_sizes = [3,4,5]
+num_filters = 512
+drop = 0.5
+
+epochs = 20
+batch_size = 30
+
+# this returns a tensor
+print("Creating Model...")
+inputs = Input(shape=(sequence_length,), dtype='int32')
+embedding = Embedding(input_dim=vocabulary_size, output_dim=embedding_dim, input_length=sequence_length)(inputs)
+reshape = Reshape((sequence_length,embedding_dim,1))(embedding)
+
+conv_0 = Conv2D(num_filters, kernel_size=(filter_sizes[0], embedding_dim), padding='valid', kernel_initializer='normal', activation='relu')(reshape)
+conv_1 = Conv2D(num_filters, kernel_size=(filter_sizes[1], embedding_dim), padding='valid', kernel_initializer='normal', activation='relu')(reshape)
+conv_2 = Conv2D(num_filters, kernel_size=(filter_sizes[2], embedding_dim), padding='valid', kernel_initializer='normal', activation='relu')(reshape)
+
+maxpool_0 = MaxPool2D(pool_size=(sequence_length - filter_sizes[0] + 1, 1), strides=(1,1), padding='valid')(conv_0)
+maxpool_1 = MaxPool2D(pool_size=(sequence_length - filter_sizes[1] + 1, 1), strides=(1,1), padding='valid')(conv_1)
+maxpool_2 = MaxPool2D(pool_size=(sequence_length - filter_sizes[2] + 1, 1), strides=(1,1), padding='valid')(conv_2)
+
+concatenated_tensor = Concatenate(axis=1)([maxpool_0, maxpool_1, maxpool_2])
+flatten = Flatten()(concatenated_tensor)
+dropout = Dropout(drop)(flatten)
+output = Dense(units=2, activation='softmax')(dropout)
+
+# this creates a model that includes
+model = Model(inputs=inputs, outputs=output)
+
+checkpoint = ModelCheckpoint('weights.{epoch:03d}-{val_acc:.4f}.hdf5', monitor='val_acc', verbose=1, save_best_only=True, mode='auto')
+adam = Adam(lr=1e-4, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
+
+model.compile(optimizer=adam, loss='binary_crossentropy', metrics=['accuracy'])
+print("Traning Model...")
+model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1, callbacks=[checkpoint], validation_data=(x_test, y_test))  # starts training
+
+# # (x_train, y_train), (x_test, y_test) = load_data('../data/filtering_comments.pos', '../data/filtering_comments.neg')
+# # (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
+# print(len(x_train), 'train sequences')
+# print(len(x_test), 'test sequences')
 
 
 
-print('Pad sequences (samples x time)')
-x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
-x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
-print('x_train shape:', x_train.shape)
-print('x_test shape:', x_test.shape)
+# print('Pad sequences (samples x time)')
+# x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
+# x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
+# print('x_train shape:', x_train.shape)
+# print('x_test shape:', x_test.shape)
 
-print('Build model...')
-model = Sequential()
+# print('Build model...')
+# model = Sequential()
 
-# we start off with an efficient embedding layer which maps
-# our vocab indices into embedding_dims dimensions
-model.add(Embedding(max_features,
-                    embedding_dims,
-                    input_length=maxlen))
-model.add(Dropout(0.2))
+# # we start off with an efficient embedding layer which maps
+# # our vocab indices into embedding_dims dimensions
+# model.add(Embedding(max_features,
+#                     embedding_dims,
+#                     input_length=maxlen))
+# model.add(Dropout(0.2))
 
-# we add a Convolution1D, which will learn filters
-# word group filters of size filter_length:
-model.add(Conv1D(filters,
-                 kernel_size,
-                 padding='valid',
-                 activation='relu',
-                 strides=1))
-# we use max pooling:
-model.add(GlobalMaxPooling1D())
+# # we add a Convolution1D, which will learn filters
+# # word group filters of size filter_length:
+# model.add(Conv1D(filters,
+#                  kernel_size,
+#                  padding='valid',
+#                  activation='relu',
+#                  strides=1))
+# # we use max pooling:
+# model.add(GlobalMaxPooling1D())
 
-# We add a vanilla hidden layer:
-model.add(Dense(hidden_dims))
-model.add(Dropout(0.2))
-model.add(Activation('relu'))
+# # We add a vanilla hidden layer:
+# model.add(Dense(hidden_dims))
+# model.add(Dropout(0.2))
+# model.add(Activation('relu'))
 
-# We project onto a single unit output layer, and squash it with a sigmoid:
-model.add(Dense(1))
-model.add(Activation('sigmoid'))
+# # We project onto a single unit output layer, and squash it with a sigmoid:
+# model.add(Dense(1))
+# model.add(Activation('sigmoid'))
 
-model.compile(loss='binary_crossentropy',
-              optimizer='adam',
-              metrics=['accuracy'])
+# model.compile(loss='binary_crossentropy',
+#               optimizer='adam',
+#               metrics=['accuracy'])
 
-model.summary()
+# model.summary()
 
-model.fit(x_train, y_train,
-          batch_size=batch_size,
-          epochs=epochs,
-          validation_data=(x_test, y_test))
+# model.fit(x_train, y_train,
+#           batch_size=batch_size,
+#           epochs=epochs,
+#           validation_data=(x_test, y_test))
---
+++ app/data_generator.py
@@ -0,0 +1,6 @@
+import numpy as np
+import keras
+
+class DataGenerator(keras.utils.Sequence):
+    def __init__(self, ):
+        
