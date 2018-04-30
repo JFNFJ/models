a436c230035ec15e0a7ea5ae20bd8fd53ec64745
Federico Bonisconti <federico.bonisconti@mercadolibre.com>
2018-04-30 17:22:17 -0300

    changed route

--- api.py
+++ api.py
@@ -1,8 +1,12 @@
-from flask import Flask
+from flask import Flask, request
 import random
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/rank", methods=['GET'])
 def just_a_random():
+    text = request.args.get('text')
+    print ("text: %s" % text)
+    if not text:
+        return "0"
     return str(random.uniform(-1, 1))
