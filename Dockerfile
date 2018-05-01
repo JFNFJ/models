d8214532f8d01f4eecf0cecc5dac3727b33d833a
Federico Bonisconti <federico.bonisconti@mercadolibre.com>
2018-05-01 00:05:26 -0300

    fixed docker

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM ubuntu:latest
 RUN apt-get update -y
 RUN apt-get install -y python-pip python-dev build-essential
 COPY . /app
+ADD requirements.txt /app
 WORKDIR /app
 RUN pip install -r requirements.txt
 ENTRYPOINT ["python"]
--- api.py
+++ api.py
@@ -6,7 +6,10 @@ app = Flask(__name__)
 @app.route("/rank", methods=['GET'])
 def just_a_random():
     text = request.args.get('text')
-    print ("text: %s" % text)
+    app.logger.debug("text: %s", text)
     if not text:
         return "0"
     return str(random.uniform(-1, 1))
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')
